79c5a8e0754ddad112d9bbfcf1a4f1b92fe1123e
tecnoweb <cuorefolle@gmail.com>
2024-04-26 21:40:51 +0200

    Update xcrud.php

--- xcrud.php
+++ xcrud.php
@@ -8,10 +8,6 @@ ini_set('display_errors', 'On'); // error showing (debug)
 // direct access to DB driver and config
 
 define('XCRUD_PATH', str_replace('\\', '/', dirname(__file__))); // str_replace - windows trick
-// includo in xcrud i file di configurazione al database
-@include (BASEPATH . "lib/config.php");
-
-
 
 
 require (XCRUD_PATH . '/xcrud_config.php'); // configuration
@@ -19,10 +15,6 @@ require (XCRUD_PATH . '/xcrud_config.php'); // configuration
 require (XCRUD_PATH . '/xcrud_db.php'); // database class
 mb_internal_encoding(Xcrud_config::$mbencoding); // sets multibyte encoding globaly
 date_default_timezone_set(@date_default_timezone_get()); // xcrud code not depends on timezone, but this can fix some warnings
-if (get_magic_quotes_runtime())
-{
-    set_magic_quotes_runtime(0);
-}
 
 class Xcrud
 {
